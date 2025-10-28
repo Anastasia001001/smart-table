603ebddcb35f13ef2f630325c0e0e157f333820d
Anastasia Tolstokoraya <atm.00@yandex.ru>
2025-10-28 19:07:52 +0300

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sp6-2_smart-table_starter
+
+Реализация умной таблицы, с применением фильтрации, сортировки и поиска
+Репозиторий: https://github.com/Anastasia001001/smart-table
--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,38 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules } from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  // @todo: #4.1 — заполнить выпадающие списки опциями
+  Object.keys(indexes).forEach((elementName) => {
+    elements[elementName].append(
+      ...Object.values(indexes[elementName]).map((name) => {
+        return `<option value="${name}">${name}</option>`;
+      })
+    );
+  });
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
-
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+  return (data, state, action) => {
+    // @todo: #4.2 — обработать очистку поля
+    if (
+      action &&
+      action.type === "click" &&
+      action.target &&
+      action.target.matches('button[name="clear"]')
+    ) {
+      const button = action.target;
+      const parentLabel =
+        button.closest(".filter-wrapper") || button.closest("label");
+      if (parentLabel) {
+        const input = parentLabel.querySelector("input");
+        if (input) {
+          input.value = "";
+        }
+      }
     }
+
+    // @todo: #4.5 — отфильтровать данные используя компаратор
+    return data.filter((row) => compare(row, state));
+  };
 }
